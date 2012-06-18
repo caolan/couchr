212a10625a54901ab8f3762d734fd22b46433fb6
Caolan McMahon <caolan@caolanmcmahon.com>
2012-06-17 19:04:36 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,25 @@
+# couchr
+
+AMD-compatible, uses jQuery.ajax to make the XHR requests but with better CouchDB
+error reporting and a simpler API.
+
+### Example
+
+```javascript
+require(['couchr'], function (couchr) {
+    couchr.get('/dbname/docid', function (err, doc) {
+        ...
+    });
+});
+```
+
+### Methods
+
+    couchr.get(url, [params], callback)
+    couchr.post(url, [data], callback)
+    couchr.head(url, callback)
+    couchr.put(url, [data], callback)
+    couchr.delete(url, [data], callback)
+
+    couchr.ajax(options, callback) - Works like the jQuery.ajax function
+    but with the callback using couchr error handling.
---
+++ couchr.js
@@ -0,0 +1,130 @@
+define('couchr', ['exports', 'jquery'], function (exports, $) {
+
+    /**
+     * Returns a function for handling ajax responses from jquery and calls
+     * the callback with the data or appropriate error.
+     *
+     * @param {Function} callback(err,response)
+     * @api private
+     */
+
+    function onComplete(options, callback) {
+        return function (req) {
+            var resp;
+            var ctype = req.getResponseHeader('Content-Type').split(';')[0];
+            if (ctype === 'application/json' || ctype === 'text/json') {
+                try {
+                    resp = $.parseJSON(req.responseText)
+                }
+                catch (e) {
+                    return callback(e);
+                }
+            }
+            else {
+                var ct = req.getResponseHeader("content-type") || "";
+                var xml = ct.indexOf("xml") >= 0;
+                resp = xml ? req.responseXML : req.responseText;
+            }
+            if (req.status === 200 || req.status === 201 || req.status === 202) {
+                callback(null, resp);
+            }
+            else if (resp && (resp.error || resp.reason)) {
+                var err = new Error(resp.reason || resp.error);
+                err.error = resp.error;
+                err.reason = resp.reason;
+                err.code = resp.code;
+                err.status = req.status;
+                callback(err);
+            }
+            else {
+                // TODO: map status code to meaningful error message
+                var err2 = new Error(
+                    req.statusText || 'Returned status code: ' + req.status
+                );
+                err2.status = req.status;
+                console.log(['status err', req, resp]);
+                callback(err2);
+            }
+        };
+    }
+
+    /**
+     * Properly encodes query parameters to CouchDB views etc. Handle complex
+     * keys and other non-string parameters by passing through JSON.stringify.
+     * Returns a shallow-copied clone of the original query after complex values
+     * have been stringified.
+     *
+     * @name stringifyQuery(query)
+     * @param {Object} query
+     * @returns {Object}
+     * @api public
+     */
+
+    exports.stringifyQuery = function (query) {
+        var q = {};
+        for (var k in query) {
+            if (typeof query[k] !== 'string') {
+                q[k] = JSON.stringify(query[k]);
+            }
+            else {
+                q[k] = query[k];
+            }
+        }
+        return q;
+    };
+
+    /**
+     * Make a request using jQuery.ajax, with some default settings and proper
+     * callback handling.
+     *
+     * @name ajax(options, callback)
+     * @param {Object} options
+     * @param {Function} callback(err,response)
+     * @api public
+     */
+
+    exports.ajax = function (options, callback) {
+        options.complete = onComplete(options, callback);
+        options.dataType = 'json';
+        $.ajax(options);
+    };
+
+
+    exports.request = function (method, url, /*optional*/data, callback) {
+        if (!callback) {
+            callback = data;
+            data = null;
+        }
+        var options = {type: method, url: url};
+        if (data) {
+            try {
+                if (method === 'GET' || method === 'HEAD') {
+                    options.data = exports.stringifyQuery(data);
+                }
+                else {
+                    options.data = JSON.stringify(data);
+                    options.processData = false;
+                    options.contentType = 'application/json';
+                }
+            }
+            catch (e) {
+                return callback(e);
+            }
+        }
+        exports.ajax(options, callback);
+    };
+
+    function makeRequest(method) {
+        return function () {
+            var args = Array.prototype.slice.call(arguments);
+            exports.request.apply(this, [method].concat(args));
+        };
+    };
+
+    exports.get = makeRequest('GET');
+    exports.post = makeRequest('POST');
+    exports.head = makeRequest('HEAD');
+    exports.put = makeRequest('PUT');
+    exports.delete = makeRequest('DELETE');
+
+});
